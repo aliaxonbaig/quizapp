5f8ebe1ea33aab886e65170c97da9e8f65cb18da
Salman Baig <aliaxon.baig@gmail.com>
2021-08-02 23:08:32 +0400

    Optimized code

--- app/Http/Livewire/UserQuizlv.php
+++ app/Http/Livewire/UserQuizlv.php
@@ -11,17 +11,15 @@ class UserQuizlv extends Component
 {
     public $section_id;
     public $size;
-    //public $userAnswer;
     public $currentQuestion;
     public $answeredQuestions = [];
     public $userAnswered = [];
-    // public $userAnswerIsCorrect;
-    //public $correctChoice;
     public $count = 0;
     public $quizid;
 
     public function showResults()
     {
+        //Easy part, need a new view to display results and redirect maybe.
         dd($this->answeredQuestions);
     }
     public function render()
@@ -36,10 +34,6 @@ class UserQuizlv extends Component
         $this->currentQuestion = $this->getNextQuestion();
     }
 
-
-    public function checkAnswer($answers)
-    {
-    }
     public function getNextQuestion()
     {
         $question = Question::where('section_id', $this->section_id)
@@ -53,7 +47,6 @@ class UserQuizlv extends Component
 
     public function nextQuestion()
     {
-
         $storeQuestion = new Quiz();
         list($answerId, $isChoiceCorrect) = explode(',', $this->userAnswered[0]);
         $storeQuestion->user_id = auth()->id();
