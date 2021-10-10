0cdaa691e953960d5d76ffac9058e05442706b32
Daniel Hofstedt <dhmhofstedt@gmail.com>
2021-10-10 14:58:35 -0500

    remove redundant caret on table filter

--- resources/views/livewire/user-datatable.blade.php
+++ resources/views/livewire/user-datatable.blade.php
@@ -29,9 +29,6 @@
                     <option value="created_at">Sign Up Date</option>
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
                 </div>
             </div>
             <div class="w-1/6 relative mx-1">
@@ -40,9 +37,6 @@
                     <option value="0">Descending</option>
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
                 </div>
             </div>
             <div class="w-1/6 relative mx-1">
@@ -53,9 +47,6 @@
                     <option>100</option>
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
                 </div>
             </div>
         </div>
